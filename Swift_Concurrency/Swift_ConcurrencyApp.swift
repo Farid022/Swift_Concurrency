3b4a69e0e9ea2e72f07a1fcb109c4256075343df
Muhammad Farid Ullah <faridkhan125janan@gmail.com>
2023-04-26 14:32:47 +0500

    Initial Commit

---
+++ Swift_Concurrency/Swift_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ConcurrencyApp.swift
+//  Swift_Concurrency
+//
+//  Created by Muhammad Farid Ullah on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
